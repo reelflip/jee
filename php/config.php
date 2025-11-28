1f3916b5bb33ecf95823fedfa30b4d11a61482a8
reelflip <36274376+reelflip@users.noreply.github.com>
2025-11-29 01:15:52 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -3,13 +3,14 @@ $servername = "82.25.121.80";
 $username = "u131922718_iitjee_tracker";
 $password = "HC2>RF|J>a!9";
 $dbname = "u131922718_iitjee_tracker";
-
+$port       = 3306;
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
 
